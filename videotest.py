df62233aa00ac553f4045173731e920f4e0fb3e0
George Vigelette <gvigelet@vigeltek.com>
2016-11-02 15:34:28 -0400

    Outline of pool

--- videotest.py
+++ videotest.py
@@ -21,7 +21,7 @@ maxValue = 255
 params = cv2.SimpleBlobDetector_Params()
     
 # Change thresholds
-params.minThreshold = 0;
+params.minThreshold = 180;
 params.maxThreshold = 256;
     
 # Filter by Area.
@@ -29,15 +29,15 @@ params.filterByArea = True
 params.minArea = 30
     
 # Filter by Circularity
-params.filterByCircularity = True
+params.filterByCircularity = False
 params.minCircularity = 0.1
     
 # Filter by Convexity
-params.filterByConvexity = True
+params.filterByConvexity = False
 params.minConvexity = 0.5
     
 # Filter by Inertia
-params.filterByInertia =True
+params.filterByInertia =False
 params.minInertiaRatio = 0.5
     
 detector = cv2.SimpleBlobDetector_create(params)
@@ -49,22 +49,17 @@ while(True):
     pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
 
     if flag:
-        keypoints = detector.detect(frame)
-        im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # keypoints = detector.detect(frame)
+        # im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         imgray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(imgray,limit,maxValue,cv2.THRESH_TOZERO)
         im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        idx =0 
-        for cnt in contours:
-            idx += 1
-            x,y,w,h = cv2.boundingRect(cnt)
-            roi=im2[y:y+h,x:x+w]
-            cv2.rectangle(im2,(x,y),(x+w,y+h),(200,0,0),2)
-
-        cv2.imshow('boxed', im2);
+        cv2.drawContours(frame, contours, -1, (0,255,0), 3)
+        
+        # cv2.imshow('GrayScale', im2);
 
         # The frame is ready and already captured
-        cv2.imshow('video', im_with_keypoints)
+        cv2.imshow('video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -73,7 +68,7 @@ while(True):
         print "Reset position"
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
         
-    time.sleep(0.02)
+    time.sleep(0.015)
 
 # When everything done, release the capture
 cap.release()
