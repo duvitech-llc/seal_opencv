3c137933156e817f822bf6d413b94980e09f20a4
George Vigelette <gvigelet@duvitech.com>
2016-11-30 00:42:07 -0500

    added some new algorithms

---
+++ background_subtraction.py
@@ -0,0 +1,112 @@
+import cv2
+import numpy as np
+import time
+import imutils
+import datetime
+
+
+# Set up the SimpleBlobdetector with default parameters.
+params = cv2.SimpleBlobDetector_Params()
+    
+params.minDistBetweenBlobs = 50.0
+params.filterByInertia = False
+params.filterByConvexity = False
+params.filterByColor = False
+params.filterByCircularity = False
+params.filterByArea = True
+params.minArea = 20.0
+params.maxArea = 500.0
+
+def sort_contours(cnts, method="left-to-right"):
+    	# initialize the reverse flag and sort index
+	reverse = False
+	i = 0
+ 
+	# handle if we need to sort in reverse
+	if method == "right-to-left" or method == "bottom-to-top":
+		reverse = True
+ 
+	# handle if we are sorting against the y-coordinate rather than
+	# the x-coordinate of the bounding box
+	if method == "top-to-bottom" or method == "bottom-to-top":
+		i = 1
+ 
+	# construct the list of bounding boxes and sort them from top to
+	# bottom
+	boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+		key=lambda b:b[1][i], reverse=reverse))
+ 
+	# return the list of sorted contours and bounding boxes
+	return (cnts, boundingBoxes)
+
+def draw_contour(image, c, i):
+	# compute the center of the contour area and draw a circle
+	# representing the center
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+ 
+	# draw the countour number on the image
+	cv2.putText(image, "#{}".format(i + 1), (cX - 20, cY), cv2.FONT_HERSHEY_SIMPLEX,
+		1.0, (255, 255, 255), 2)
+ 
+	# return the image with the contour number drawn on it
+	return image
+
+def get_frame(cap, scaling_factor):
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=scaling_factor, 
+            fy=scaling_factor, interpolation=cv2.INTER_AREA)
+    return frame
+
+if __name__=='__main__':
+    cap = cv2.VideoCapture("videos/Nov_19.mov")
+    while not cap.isOpened():
+        cap = cv2.VideoCapture("videos/Nov_19.mov")
+        cv2.waitKey(1000)
+        print ("Wait for the header")
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    bgSubtractor = cv2.createBackgroundSubtractorMOG2()
+        
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    history = 100
+
+    while True:
+        frame = get_frame(cap, 1.0)
+        mask = bgSubtractor.apply(frame, learningRate=1.0/history)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+        keypoints = detector.detect(mask & frame)
+        im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        
+        imgray = cv2.cvtColor(mask & frame,cv2.COLOR_BGR2GRAY)
+        ret,thresh = cv2.threshold(imgray,127,255,0)
+        
+        im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+        # loop over the contours
+        for c in contours:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 20:
+                continue
+    
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = "Occupied"
+
+        cv2.imshow('Original', frame)
+        cv2.imshow('Moving Objects', mask & frame)
+        c = cv2.waitKey(10)
+        if c == 27:
+            break
+        
+        time.sleep(0.020)
+
+    cap.release()
+    cv2.destroyAllWindows()
--- videotest.py
+++ videotest.py
@@ -1,41 +1,26 @@
 #!/usr/bin/env python
 
-from imutils.object_detection import non_max_suppression
-from imutils import paths
 import cv2
 import numpy as np
 import time
 import imutils
 import datetime
 
-blobsNotFound = []
-pool_zone = None
-firstFrame = None
-prevFrame = None
-# Set up the SimpleBlobdetector with default parameters.
-params = cv2.SimpleBlobDetector_Params()
+
+scaling_factor = 1.0
     
-# Change thresholds
-params.minThreshold = 0;
-params.maxThreshold = 256;
-    
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 30
-    
-# Filter by Circularity
-params.filterByCircularity = True
-params.minCircularity = 0.1
-    
-# Filter by Convexity
-params.filterByConvexity = True
-params.minConvexity = 0.5
-    
-# Filter by Inertia
-params.filterByInertia =True
-params.minInertiaRatio = 0.5
-    
-detector = cv2.SimpleBlobDetector_create(params)
+
+def frame_diff(prev_frame, cur_frame, next_frame):
+    diff_frames1 = cv2.absdiff(next_frame, cur_frame)
+    diff_frames2 = cv2.absdiff(cur_frame, prev_frame)
+    return cv2.bitwise_and(diff_frames1, diff_frames2)
+
+def get_frame(cap):
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=scaling_factor, 
+            fy=scaling_factor, interpolation=cv2.INTER_AREA)
+    return cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+
 
 cap = cv2.VideoCapture("videos/Nov_19_2.mov")
 while not cap.isOpened():
@@ -45,42 +30,32 @@ while not cap.isOpened():
 
 total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) - 5
 
+prev_frame = get_frame(cap) 
+cur_frame = get_frame(cap) 
+next_frame = get_frame(cap) 
+
 while(True):
-    flag, frame = cap.read()
 
     text = "Unoccupied"
-    pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
 
+    cv2.imshow("Object Movement", frame_diff(prev_frame, cur_frame, next_frame))
 
-    if flag:
+    # draw the text and timestamp on the frame
+    cv2.putText(cur_frame, "Status: {}".format(text), (10, 20),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(cur_frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+        (10, cur_frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
         
-        myblur = cv2.blur(frame, (8, 8))
-        im_color = cv2.applyColorMap(myblur, cv2.COLORMAP_PINK)
-        
-        # Detect blobs.
-        img_th = cv2.adaptiveThreshold(im_color,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-        contours, hierarchy = cv2.findContours(img_th, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
+    cv2.imshow('original', cur_frame)
         
-        # draw the text and timestamp on the frame
-        cv2.putText(frame, "Status: {}".format(text), (10, 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-            
-        prevFrame = frame
-
-        cv2.imshow('original', frame)
-        cv2.imshow("Frame", im_color)
-        
+    prev_frame = cur_frame
+    cur_frame = next_frame 
+    next_frame = get_frame(cap)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    if pos_frame >= total_frames:
-        print ("Reset position")
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        
     time.sleep(0.015)
 
 # When everything done, release the capture
