b338ff746f4339509e8dc27cf34c90066e943d57
George Vigelette <gvigelet@duvitech.com>
2016-12-01 00:04:14 -0500

    tracking a little better

--- background_subtraction.py
+++ background_subtraction.py
@@ -3,7 +3,57 @@ import numpy as np
 import time
 import imutils
 import datetime
+from matplotlib import pyplot as plt
 
+
+class Point:
+    """ Point class represents and manipulates x,y coords. """
+
+    def __init__(self, x=0, y=0):
+        """ Create a new point at x, y """
+        self.x = x
+        self.y = y
+
+    def print_point(pt):
+        print("({0}, {1})".format(pt.x, pt.y))
+
+
+class Rectangle:
+    """ A class to manufacture rectangle objects """
+
+    def __init__(self, posn, w, h):
+        """ Initialize rectangle at posn, with width w, height h """
+        self.corner = posn
+        self.width = w
+        self.height = h
+
+    def __str__(self):
+        return  "({0}, {1}, {2})".format(self.corner, self.width, self.height)  
+
+    def grow(self, delta_width, delta_height):
+        """ Grow (or shrink) this object by the deltas """
+        self.width += delta_width
+        self.height += delta_height
+
+    def move(self, dx, dy):
+        """ Move this object by the deltas """
+        self.corner.x += dx
+        self.corner.y += dy
+
+    def draw(self, target):
+        """ Draw rectangle on image """
+        cv2.rectangle(target, (self.corner.x,self.corner.y), (self.corner.x + self.width, self.corner.y + self.height), (0,0,255),2)
+    
+    def contains(self, rect):
+        """ Determing if rectangle resides inside this Rectangle """
+        if((rect.corner.x >=self.corner.x and rect.corner.y >= self.corner.y) and \
+            (((rect.corner.x + rect.width) <= (self.corner.x + self.width)) and ((rect.corner.y + rect.height) <= (self.corner.y + self.height)))):
+            return True
+        else:
+            return False
+
+
+currtrack = []
 
 # Set up the SimpleBlobdetector with default parameters.
 params = cv2.SimpleBlobDetector_Params()
@@ -60,6 +110,58 @@ def get_frame(cap, scaling_factor):
             fy=scaling_factor, interpolation=cv2.INTER_AREA)
     return frame
 
+def searchForMovement(resThresh, cameraFeed):    
+    global count
+    global currtrack
+    minx = None
+    miny = None
+    maxw = None
+    maxh = None
+    
+    temp = None
+    objectDetected = False
+    temp = resThresh
+    _, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    if(contours.__len__()>1):
+        objectDetected = True
+    else:
+        objectDetected = False
+
+    if(objectDetected):        
+        # loop over the contours
+        for c in contours:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 10:
+                continue
+    
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x, y, w, h) = cv2.boundingRect(c)
+            if minx is None:
+                minx = x 
+                miny = y 
+                maxw = w 
+                maxh = h
+            else:
+                if x < minx:
+                    minx = x 
+                if y < miny:
+                    miny = y 
+                if w > maxw:
+                    maxw = w 
+                if h > maxh:
+                    maxh = h 
+        
+        if minx is not None:
+            # origin       
+            xpos = minx + maxw/2
+            ypos = miny + maxh/2
+            br = Rectangle(Point(xpos,ypos), maxw, maxh)
+            currtrack.append(br)
+            cv2.rectangle(cameraFeed, (minx, miny), (minx + maxw, miny + maxh), (0, 255, 0), 2)
+
+    return objectDetected 
+
 if __name__=='__main__':
     cap = cv2.VideoCapture("videos/Nov_19.mov")
     while not cap.isOpened():
@@ -72,7 +174,7 @@ if __name__=='__main__':
         
     detector = cv2.SimpleBlobDetector_create(params)
 
-    limit = 195
+    limit = 184
     maxValue = 255
     history = 100
     firstFrame = True
@@ -86,7 +188,7 @@ if __name__=='__main__':
             imp, ctp, hierarchy = cv2.findContours(thp,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
             firstFrame = False
 
-        cv2.drawContours(frame, ctp, -1, (180,0,0), 3)
+        # cv2.drawContours(frame, ctp, -1, (180,0,0), 3)
 
         mask = bgSubtractor.apply(frame, learningRate=1.0/history)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -98,21 +200,8 @@ if __name__=='__main__':
         imgray = cv2.cvtColor(mask & frame,cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(imgray,127,255,0)
         
-        # less blob delete pool
-        im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-        # loop over the contours
-        for c in contours:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < 20:
-                continue
-    
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = "Occupied"
-
+        searchForMovement(thresh, frame)
+        
         cv2.imshow('Original', frame)
         cv2.imshow('Moving Objects', mask & frame)
         c = cv2.waitKey(10)
